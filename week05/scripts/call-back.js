4201e0f7f04695d8a2a142274b1bf5ca7bd193fe
luyinga <lumbaleluyinga2018@gmail.com>
2025-11-30 20:16:56 +0200

    Week05 added

---
+++ week05/scripts/call-back.js
@@ -0,0 +1,11 @@
+function calculate(a, b, callback) {
+  callback(a + b);
+}
+
+function displayResult(result) {
+  console.log('The result is: ' + result);
+}
+
+//Call a function named calculate and pass it the arguments to support the console output of the following equation: 2 + 3. You need to pass three arguments to the calculate function.
+//Check Your Understanding
+calculate(2, 3, displayResult)
